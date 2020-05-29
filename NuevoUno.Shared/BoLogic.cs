d72611e156d359d8efab2980546870d9fa91021c
Jose Ojeda <joche.ojeda@bitframeworks.com>
2020-05-29 15:11:45 -0700

    Add project files.

---
+++ NuevoUno.Shared/BoLogic.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Data.Filtering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstXafProject.Orm
+{
+    public class BoLogic
+    {
+        public static void CreateInvoice(DateTime InvoiceData,int CustomerCode)
+        {
+            using (var UoW = XpoHelper.CreateUnitOfWork())
+            {
+                var CustomerInstance=UoW.FindObject<Customer>(new BinaryOperator(nameof(Customer.Code), CustomerCode));
+                Invoice invoice = new Invoice(UoW) {Date=InvoiceData, Customer= CustomerInstance };
+                if (UoW.InTransaction)
+                    UoW.CommitChanges();
+            }
+
+        }
+
+        public static void CreateCustomer( int CustomerCode,string Name)
+        {
+            using (var UoW = XpoHelper.CreateUnitOfWork())
+            {
+                var CustomerInstance = UoW.FindObject<Customer>(new BinaryOperator(nameof(Customer.Code), CustomerCode));
+
+                if(CustomerInstance==null)
+                {
+                    CustomerInstance = new Customer(UoW) { Code = CustomerCode, Name = Name };
+                }
+
+                if (UoW.InTransaction)
+                    UoW.CommitChanges();
+            }
+
+        }
+
+    }
+}
---
+++ NuevoUno.Shared/Views/Shell.xaml.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DevExpress.Xpo;
+using FirstXafProject.Orm;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NuevoUno.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shell
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+            this.SimpleTest.Click += ChangeText;
+            this.LoadData.Click += LoadDataFromServer;
+        }
+        private async void ChangeText(object sender, RoutedEventArgs e)
+        {
+           
+        }
+            private async void LoadDataFromServer(object sender, RoutedEventArgs e)
+        {
+           
+            try
+            {
+                var Ds = new DevExpress.Xpo.DB.InMemoryDataStore(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema, true);
+                XpoHelper.InitXpo(Ds);
+
+                BoLogic.CreateCustomer(1, "Joche Ojeda");
+                BoLogic.CreateCustomer(2, "Jaime Macias");
+                BoLogic.CreateCustomer(3, "Javier Columbie");
+                BoLogic.CreateCustomer(4, "Rafael Gonzales");
+
+                BoLogic.CreateInvoice(new System.DateTime(2020, 1, 1), 1);
+
+                var Count = XpoHelper.CreateUnitOfWork().Query<Invoice>().Count();
+                Console.WriteLine("Its working");
+                this.LoadData.Content = "Its working";
+                XPCollection<Customer> customer = new XPCollection<Customer>(XpoHelper.CreateUnitOfWork());
+                this.MasterListView.ItemsSource = customer;
+            }
+            catch (Exception ex)
+            {
+                var messsa = ex.Message;
+                throw ex;
+            }
+
+        }
+    }
+}
